f9189292a69c7bc9226243bfbf23ab83035e7a4f
Brantley <754867778@qq.com>
2024-12-15 19:41:42 +0800

    feat: 饿汉式单例模式

---
+++ basics/src/main/java/com/quick/design/basics/structure/HungrySingleton.java
@@ -0,0 +1,33 @@
+package com.quick.design.basics.structure;
+
+/**
+ * @Author vic
+ * @Date 2024/12/15 19:34
+ * @Description 饿汉式单例模式
+ * <p>
+ *     特点：
+ *          1、类加载时就创建实例，无需加锁，线程安全。
+ *          2、即使没有被使用，实例也会被创建。
+ *     适用场景：
+ *          1、对象较小
+ * </p>
+ */
+public class HungrySingleton {
+
+    private static HungrySingleton INSTANCE = new HungrySingleton();
+
+    /**
+     * 单例模式不能被外部实例化
+     */
+    private HungrySingleton() {}
+
+    /**
+     * 提供唯一入口访问唯一的实例对象
+     *
+     * @return
+     */
+    public static HungrySingleton getInstance() {
+        return INSTANCE;
+    }
+
+}
